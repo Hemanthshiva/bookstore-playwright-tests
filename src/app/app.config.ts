78c387a706b9306aa12465ae28ab2fd840dabeab
Hemanth Shivashankarappa <hshivashankarappa@gmail.com>
2026-03-15 17:39:00 +0000

    Fix the SSR configuration error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    provideAnimations()
+    provideAnimationsAsync()
   ]
 };
