68061cb59dd4842046ab9a760dd68ea7259f142b
Hemanth Shivashankarappa <hshivashankarappa@gmail.com>
2026-03-15 19:25:43 +0000

    Fixing the errors on CI run

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,5 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+// Server routes configuration
+// In development (ssr: false), these routes are mostly for documentation
+// In production (ssr: true), they define how routes should be pre-rendered
 export const serverRoutes: ServerRoute[] = [
   {
     path: '',
